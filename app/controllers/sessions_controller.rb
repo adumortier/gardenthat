32a7f632b2f4713ac8980b811a1beba25aeec357
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-08 17:49:06 -0600

    Add the googleauth method to the sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def googleAuth
+    access_token = request.env["omniauth.auth"]
+    user = User.from_omniauth(access_token)
+    log_in(user)
+
+    user.google_token = access_token.credentials.token
+    
+    refresh_token = access_token.credentials.refresh_token
+    user.google_refresh_token = refresh_token if refresh_token.present?
+    user.save
+    redirect_to root_path
+  end
+
+end
