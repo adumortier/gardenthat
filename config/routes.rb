cab9544b342b687ad3eb912df6fdd2d1b6c9311f
Nathan Keller <keller.nathan@gmail.com>
2020-04-08 13:07:05 -0600

    Adjust root route to root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'welcome#index'
+  root 'welcome#index'
 	get '/about', to: 'about#show'
 	get '/register', to: 'user#new'
 	get '/login', to: 'session#new'
