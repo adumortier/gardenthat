ee20bd935879955274a9e287a6783d3b70cdc168
Nathan Keller <keller.nathan@gmail.com>
2020-04-07 19:55:24 -0600

    Add test for welcome page. Minimal content

---
+++ spec/welcome/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "welcome index page", type: :feature do
+  it "can see welcome message" do
+
+    visit "/"
+
+    expect(page).to have_content("Welcome To Gard(in) That!")
+  end
+end
