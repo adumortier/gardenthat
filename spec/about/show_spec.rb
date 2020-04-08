22675318f26dcb8da1b9d1d3465d6439d4b0638a
Nathan Keller <keller.nathan@gmail.com>
2020-04-08 13:35:16 -0600

    Add test for about page content headers

---
+++ spec/about/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "about show page", type: :feature do
+  it "has content about the webiste" do
+
+    visit '/'
+
+    click_link 'About'
+    
+    expect(page).to have_content("About Gard(in) That")
+    expect(page).to have_content("How To Use This Site")
+    expect(page).to have_link("Trefle")
+    expect(page).to have_content("Contact Us")
+  end
+end
