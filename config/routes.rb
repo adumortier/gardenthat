2d3b8bb9c14afa182f5db93f1130f481170b9a4e
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-08 19:39:56 -0600

    Add registration and oauth redirect back/failure routes
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/about', to: 'about#show'
-  get 'auth/:provider/callback', to: 'sessions#googleAuth'
-  get 'auth/failure', to: redirect('/')
+  get '/register', to: 'users#new'
+  
+  get '/auth/:provider/callback', to: 'sessions#googleAuth'
+  get '/auth/failure', to: redirect('/')
 end
