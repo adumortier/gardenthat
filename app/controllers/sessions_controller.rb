f33bc8921ce2f7febd74717cbe2c2c6fcebe5bea
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-08 20:00:28 -0600

    Add a destroy method to clear the session[:user_id] when signing out
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
     user.save
     redirect_to root_path
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+  
 end
