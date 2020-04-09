0c2699894ad28e030556aeb3a044de315267c8a6
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-08 19:59:39 -0600

    Add a route to clear the session[:user_id] when signing out
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/about', to: 'about#show'
   get '/register', to: 'users#new'
+  delete '/logout', to: 'sessions#destroy'
   
   get '/auth/:provider/callback', to: 'sessions#googleAuth'
   get '/auth/failure', to: redirect('/')
