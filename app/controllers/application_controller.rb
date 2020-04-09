aa3de238e403e50a8670d39b2e3c200528554530
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-08 19:58:59 -0600

    Add a method to return the current_user across the app
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
