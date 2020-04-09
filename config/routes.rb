709f386db3fcd3fdb1c2141d5c07833bc554aa81
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-09 15:40:56 -0600

    Add a route for login
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/about', to: 'about#show'
   get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
   delete '/logout', to: 'sessions#destroy'
   
   get '/auth/:provider/callback', to: 'sessions#googleAuth'
