b923f50876c54a4b24d073ce053081cdc97398f9
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-11 10:40:34 -0600

    Remove empty line
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+
+
 require 'vcr'
 require 'webmock/rspec'
 
