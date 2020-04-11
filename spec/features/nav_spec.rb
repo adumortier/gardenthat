5903caf3c03dc04694aad07da77826c295e719ba
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-11 14:15:50 -0600

    Add a logged in user to access the signout link
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- spec/features/nav_spec.rb
+++ spec/features/nav_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe 'vister' do
+describe 'visitor' do
   it 'about' do
 		visit '/'
-		expect(page).to have_content('About')
+    expect(page).to have_content('About')
 		click_on 'About'
 		expect(current_path).to eq('/about')
 	end
@@ -14,8 +14,15 @@ describe 'vister' do
 		expect(current_path).to eq('/login')	
 	end
 
-	it 'sign out' do
-		visit '/'
+  it 'sign out' do
+    user1 = User.create!( email: 'gardenthat@gmail.com', 
+                          name: 'gardenthat', 
+                          google_token: 'temp',
+                          zip_code: '02139'
+                        )
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    visit '/'
+    expect(page).to have_link('My Garden')
 		click_on 'Sign Out'
 		expect(current_path).to eq('/')	
 	end
