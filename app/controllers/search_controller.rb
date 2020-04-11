08f3140ae662b6838f0d80b14386fe75d55a1acc
Nathan Keller <keller.nathan@gmail.com>
2020-04-11 17:33:04 -0600

    Added show action to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,17 @@ class SearchController < ApplicationController
   def index
     conn = Faraday.new "https://plantmicroservice.herokuapp.com/api/"
     request = params['search'].strip.gsub(' ','-')
-    response = conn.get(request) 
+    response = conn.get(request)
     json = JSON.parse(response.body, symbolize_names: true)
     @plant = Plant.new(json)
   end
 
-end 
+  def show
+    conn = Faraday.new "https://plantmicroservice.herokuapp.com/api/"
+    request = params['format'].strip.gsub(' ','-')
+    response = conn.get(request)
+    json = JSON.parse(response.body, symbolize_names: true)
+    @plant = Plant.new(json)
+  end
+
+end
