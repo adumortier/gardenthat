9c116b3e2d8c57551f557b040b6c60f443be745d
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-11 10:35:12 -0600

    Remove validatation of google_refresh_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email, :google_token, :google_refresh_token
-  # , :zip_code
-
+  validates_presence_of :name, :email, :google_token
+  
   def self.from_omniauth(auth)
-    where(email: auth.info.email).first_or_initialize do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
+    where(email: auth["info"]["email"]).first_or_initialize do |user|
+      user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
     end
   end
 
