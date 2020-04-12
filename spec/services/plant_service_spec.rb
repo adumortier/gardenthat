fe023ef2251d507959cb9f0679bcae37026fb938
Nathan Keller <keller.nathan@gmail.com>
2020-04-12 13:56:19 -0600

    Add test for service spec.

---
+++ spec/services/plant_service_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+
+RSpec.describe 'plant service index', :type => :request do
+  before do
+    get '/search', params: { 'search' => 'tomato' }
+  end
+
+  it 'returns status code 200', :vcr do
+    expect(response).to have_http_status(:success)
+  end
+end
+
+describe 'plant service show', :type => :request do
+  before do
+    get '/search/details', params: { 'format' => 'tomato' }
+  end
+
+  it 'returns status code 200', :vcr do
+    expect(response).to have_http_status(:success)
+  end
+end
