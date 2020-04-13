bd261182af2fd00122206a34df8455e83ed72dc8
Nathan Keller <keller.nathan@gmail.com>
2020-04-13 17:12:16 -0600

    Add test for plant index

---
+++ spec/features/plants/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Browse All Plants Page" , type: :feature do
+  it "can list all avaiable plants to browse", :vcr do
+    visit '/'
+
+    click_on 'Browse All Plants'
+
+    expect(current_path).to eq('/plants')
+
+    within('.plant_info') do
+      expect(first('.name').text).to_not be_empty
+      expect(first('.description').text).to_not be_empty
+    end
+    save_and_open_page
+    # expect(page).to have_content("Displaying 25 Results")
+    # expect(page).to have_link('Next Page')
+  end
+end
