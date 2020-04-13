746f6fb24ad541c742655023f24064ecdac0eb67
Nathan Keller <keller.nathan@gmail.com>
2020-04-13 17:13:08 -0600

    Add link to browse all plants

--- app/services/plant_service.rb
+++ app/services/plant_service.rb
@@ -1,11 +1,16 @@
 class PlantService
   def conn
-    conn = Faraday.new "https://plantmicroservice.herokuapp.com/api/"
+    Faraday.new "https://plantmicroservice.herokuapp.com/api/"
   end
 
   def get_search_info(search_param)
     request = search_param.strip.gsub(' ','-')
     response = conn.get(request)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_all_plants
+    require "pry"; binding.pry
+    JSON.parse(new_conn.body, symbolize_names: true)
   end
 end
