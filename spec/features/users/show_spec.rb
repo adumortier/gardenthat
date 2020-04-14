e751a6a8149bb91b3dd46c326f359b6a570bd593
Nathan Keller <keller.nathan@gmail.com>
2020-04-14 16:43:31 -0600

    Add sad path test to user show page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "As a registered user " , type: :feature do
+RSpec.describe "User Profile Page" , type: :feature do
   before(:each) do
     @user1 = User.create!( email: 'gardenthat@gmail.com',
                           name: 'gardenthat',
@@ -11,7 +11,7 @@ RSpec.describe "As a registered user " , type: :feature do
 
   end
 
-  it "can see its porofile page" do
+  it "can see its profile page" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
     visit '/'
@@ -22,5 +22,17 @@ RSpec.describe "As a registered user " , type: :feature do
     expect(page).to have_content(@user1.email)
     expect(page).to have_content(@user1.name)
     expect(page).to have_content(@user1.zip_code)
+  end
+
+  it "renders a 404 if you try and visit the page without signing in" do
+    user = User.create!( email: 'thing@gmail.com',
+                          name: 'thatthing',
+                          zip_code: '02300',
+                          google_token: 'temp',
+                          google_refresh_token: 'temp'
+                        )
+    visit "/profile/#{user.id}"
+
+    expect(page).to have_content("The page you were looking for doesn't exist")
   end
 end
