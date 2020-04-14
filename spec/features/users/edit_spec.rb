f2b33fcb75e6a056f697075f8d4fe9f8f88e4c27
Nathan Keller <keller.nathan@gmail.com>
2020-04-14 17:14:00 -0600

    User Edit spec. happy path

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -14,20 +14,19 @@ RSpec.describe "Edit Profile " , type: :feature do
   it "can successfully edit thier profile" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
-    visit '/profile'
+    visit "/profile/#{@user1.id}"
 
     click_on 'Edit Profile'
 
-    expect(current_path).to eq('/profile/edit')
+    expect(current_path).to eq("/profile/#{@user1.id}/edit")
 
     expect(page).to have_selector("input[value='02300']")
 
     fill_in :zip_code, with: "80003"
 
-    click_button "Update Profile"
+    click_button 'Update Profile'
 
-    expect(current_path).to eq("/profile#{@user1.id}")
-    expect(page).to have_content("Your Profile has been Updated!")
-    expect(page).to have_content("80003")
+    expect(current_path).to eq("/profile/#{@user1.id}")
+    expect(page).to have_content('80003')
   end
 end
