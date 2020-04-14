3422c613bf044405d1d45164983d42f5f874534f
Nathan Keller <keller.nathan@gmail.com>
2020-04-14 16:31:15 -0600

    Add show action to profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,18 @@ class ProfilesController < ApplicationController
   def new
   end
 
+  def show
+    if current_user.nil?
+      render file: "/public/404"
+    else
+      @user = User.find(current_user.id)
+    end
+  end
+
+  # def edit
+  #   @user = User.find(session[:user_id])
+  # end
+
   def create
     if current_user
       current_user.update(zip_code: params["zip_code"])
