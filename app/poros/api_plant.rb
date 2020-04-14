fd0ade8ef5cbdf88494575757730cc1f16c71c1c
Nathan Keller <keller.nathan@gmail.com>
2020-04-14 14:39:53 -0600

    Add defualt image to APLI plant Poro

--- app/poros/api_plant.rb
+++ app/poros/api_plant.rb
@@ -1,18 +1,36 @@
 class ApiPlant
 
-  attr_reader :name, :perennial, :first_harvest, :last_harvest, :description, :sun, :image, :height, :spread, :sowing, :spacing
+  attr_reader :name,
+              :perennial,
+              :first_harvest,
+              :last_harvest,
+              :description,
+              :sun,
+              :image,
+              :height,
+              :spread,
+              :sowing,
+              :spacing
 
-  def initialize(info) 
+  def initialize(info)
     @name = info[:name]
     @perennial = info[:perennial]
     @first_harvest = info[:first_harvest]
     @last_harvest = info[:last_harvest]
     @description = info[:description]
     @sun = info[:sun]
-    @image = info[:image]
+    @image = default_image(info[:image])
     @height = info[:height]
     @spread = info[:spread]
     @sowing = info[:sowing]
     @spacing = info[:spacing]
-  end   
+  end
+
+  def default_image(image)
+    if image.nil?
+      image = "https://www.aisd.net/burgin-elementary/wp-content/files/sites/54/2018/08/No-image-available.jpg"
+    else
+      image
+    end
+  end
 end
