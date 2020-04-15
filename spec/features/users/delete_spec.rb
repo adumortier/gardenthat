20d831197e9b53b1296a675c827ba9ebcdf06c94
Nathan Keller <keller.nathan@gmail.com>
2020-04-15 10:25:17 -0600

    Create delete spec

---
+++ spec/features/users/delete_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Delete Profile" , type: :feature do
+  it 'can deletes a profile' do
+    user = User.create!( email: 'gardenthat@gmail.com',
+                          name: 'gardenthat',
+                          zip_code: '02300',
+                          google_token: 'temp',
+                          google_refresh_token: 'temp'
+                        )
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/profile/#{user.id}"
+
+    expect(User.all.last).to eq(user)
+
+    click_link 'Delete Profile'
+
+    expect(current_path).to eq('/')
+
+    expect(page).to have_content("Profile Deleted")
+    expect(User.all).to eq([])
+  end
+end
