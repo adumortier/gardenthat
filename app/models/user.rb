f45d3673fc9f1a9249ad331f78c2ccf248b076bf
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-15 19:08:39 -0600

    Remove name validation (name not returned from new googleoauth call)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :gardens, dependent: :destroy
 	has_many :garden_plants, dependent: :destroy
-  validates_presence_of :name, :email, :google_token
+  validates_presence_of :email, :google_token
   validates :email, uniqueness: true
 	validates :zip_code, length: {minimum: 5, maximum: 5}, allow_blank: true
 
