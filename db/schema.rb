65c10b2b95b30362158e17df701e3d360a9ef35a
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-15 19:14:10 -0600

    Add the calendar_id column to the Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_211316) do
+ActiveRecord::Schema.define(version: 2020_04_15_224947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_211316) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "calendar_id"
   end
 
   add_foreign_key "garden_plants", "gardens"
