baf618e84458a1f04cea651a20c3ea4eb23013d2
Paul Debevec <paulmdebevec@gmail.com>
2020-04-16 20:51:23 -0600

    Add catch for blank search

--- app/services/plant_service.rb
+++ app/services/plant_service.rb
@@ -4,12 +4,14 @@ class PlantService
   end
 
   def get_individual_plant_details(search_params)
-    response = conn.get("/plant/#{search_params}")
+    request = search_params.strip.gsub(' ','-').downcase
+    response = conn.get("/plant/#{request}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_multiple_info(search_param)
-    request = search_param.strip.gsub(' ','-')
+  def get_multiple_info(search_params)
+    search_params = "A" if search_params == ""
+    request = search_params.strip.gsub(' ','-').downcase
     response = conn.get("/api/#{request}")
     JSON.parse(response.body, symbolize_names: true)
   end
