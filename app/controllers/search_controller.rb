92c512d7b414011d60fc0e161c1b1ebbe40b3cae
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-17 07:45:50 -0600

    Replace instance variables by locals

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,11 +5,12 @@ class SearchController < ApplicationController
     @plants = result.map do |plant|
       ApiPlant.new(plant)
     end
+    render locals: {plants: @plants}
   end
 
   def show
     result = PlantService.new.get_individual_plant_details(params['format'])
     return @error = "Bad Plant" if result == "Bad Plant"
-    @plant = ApiPlant.new(result)
+    render locals: {plant: ApiPlant.new(result)}
   end
 end
