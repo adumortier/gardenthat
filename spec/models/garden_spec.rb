a81ec03715f9177d69f0c2fdce8cd26dccdc2cb9
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-16 19:06:55 -0600

    Require rails_helper for the spec

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -1,9 +1,14 @@
+require 'rails_helper'
+
 describe Garden, type: :model do
-        describe "validations" do
-                it { should validate_presence_of :name }
-        end
+  
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
   describe 'relationships' do
-                it { should belong_to :user }
-                it { should have_many(:plants).through(:garden_plants)}
-        end	
+    it { should belong_to :user }
+    it { should have_many(:plants).through(:garden_plants)}
+  end	
+
 end
