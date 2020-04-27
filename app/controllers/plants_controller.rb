397dbb3738b897cdea85a824a8670aeb34636cff
adumortier <42797894+adumortier@users.noreply.github.com>
2020-04-27 14:48:07 -0600

    Update plants_controller.rb

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,6 @@
 class PlantsController < ApplicationController
   def index
-    json ||= PlantService.get_all_plants
+    json = PlantService.get_all_plants
     plants = json.map { |plant| ApiPlant.new(plant) }
     render locals: {plants: plants.paginate(:page => params[:page], :per_page => 24)}
   end
